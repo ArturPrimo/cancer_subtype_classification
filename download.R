ba01027f1fcce28d080493187634b3986cee5f25
Artur Primo <arturprimo@vt.edu>
2026-04-06 18:23:39 -0400

    Included gitignore

--- download.R
+++ download.R
@@ -4,6 +4,8 @@
 #install.packages("rlang")
 #update.packages(ask = FALSE)
 
+setwd("C:/Users/artur/OneDrive/Documents/VT/Junior/Spring/ECE4424/cancer_subtype_classification")
+
 library(TCGAbiolinks)
 library(DT)
 
@@ -14,5 +16,7 @@ DT::datatable(
   options = list(scrollX = TRUE, keys = TRUE, pageLength = 5),
   rownames = FALSE
 )
+
+dir.create("data", showWarnings = FALSE, recursive = TRUE)
 
 write.csv(subtypes, "data/cancer_subtypes.csv", row.names = FALSE)
